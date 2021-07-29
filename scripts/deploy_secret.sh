e2ec5fcc100ba5ba23aa768fb15964f46bee8d7e
akihokurino <aki030402@gmail.com>
2021-07-30 01:12:20 +0900

    fixed

--- scripts/deploy_secret.sh
+++ scripts/deploy_secret.sh
@@ -12,8 +12,10 @@ echo "--------------- start update secret ---------------"
 
 gcloud container clusters get-credentials app-cluster --zone=asia-northeast1-a
 kubectl delete secret env
+kubectl delete secret batch-env
 kubectl create secret generic gcp-credentials --from-file=credentials.json=${APP_ROOT}/gcp.${ENV}.json
 kubectl create secret generic firebase-credentials --from-file=credentials.json=${APP_ROOT}/firebase.${ENV}.json
 kubectl create secret generic env --from-file=env=${APP_ROOT}/.env.${ENV}
+kubectl create secret generic batch-env --from-file=batch-env=${APP_ROOT}/.env.batch.${ENV}
 
 echo "--------------- complete update secret ---------------"
